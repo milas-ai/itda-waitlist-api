3f2e388785acdc705018e9138f0c02a128816aea
Diogo Ribeiro <diogoribfaria@gmail.com>
2025-11-03 00:52:02 -0300

    Add default route for HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use actix_web::{get, web, App, HttpServer, Responder, middleware::Logger};
+use actix_web::{get, App, HttpServer, Responder, middleware::Logger};
+use std::env;
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+#[get("/")]
+async fn index() -> impl Responder {
+    let feed_url = format!(
+        "https://isthereanydeal.com/feeds/waitlist.rss?token={}",
+        env::var("WAITLIST_RSS_TOKEN").unwrap()
+    );
+    format!("Waitlist RSS Feed URL: {feed_url}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
+    if let Err(_) = env::var("WAITLIST_RSS_TOKEN") {
+        panic!("WAITLIST_RSS_TOKEN must be set in the environment");
+    }
+    
     env_logger::init();
-
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
-            .service(greet)
+            .service(index)
     })
     .bind(("127.0.0.1", 8286))?
     .run()
